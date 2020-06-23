a29dea94aefe7d4e91cb967f10971c2819722bef
Mario McGee <mario.mcgee@students.makeschool.com>
2020-06-22 22:20:37 -0700

    finished

--- music/models.py
+++ music/models.py
@@ -10,8 +10,8 @@ class Musician(models.Model):
 class Album(models.Model):
     name = models.CharField(max_length=100)
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    genre = models.CharField(max_length=50)
     publish_date = models.DateField(blank=True, null=True)
+
 
     def __str__(self):
       return self.name
@@ -22,6 +22,8 @@ class Song(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True)
+    genre = models.CharField(max_length=50, null=True)
+   
 
     def __str__(self):
         return self.name
--- music/urls.py
+++ music/urls.py
@@ -3,12 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('classical/', views.classical_songs, name='classical'),
   path('',views.artist_list_view, name='artist'),
   path('artist/<int:artist_id>/', views.artist_detail_view, name='artists'),
   path('album/<int:album_id>/', views.album_detail, name='album'),
   path('song/<int:song_id>/', views.song_detail, name='song'),
- 
-  
+   
 ]
